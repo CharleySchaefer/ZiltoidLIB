f9ddf9b3e6625469c9ae6ca9aa16f67b433a7437
Charley Schaefer <charley.schaefer@gmail.com>
2018-06-19 13:59:54 +0100

    commit message

---
+++ StringOperations/StringOperations.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+void	numStr(char* , int , int );
+int	countStr(char* , int );
+int     fileNumber(char *, int, int, char *);
+int     getWord(char *, char *, int *);
+int     countWords(char *, char *, int *);
+
+
---
+++ StructureFactor/StructureFactor.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <complex.h>
+#include <math.h>
+#include "../Fourier/Fourier.h"
+
+
+int calculateStructureFactor(int, int, double, double **, complex **, int, complex *, double *, double *);
+
