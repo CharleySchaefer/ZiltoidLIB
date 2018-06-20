ded7d443211606af8ea5ed46dacc87dad40e19d6
Charley Schaefer <charley.schaefer@gmail.com>
2018-06-20 15:53:38 +0100

    ADDED ZiltoidLIB_LOADED definition in main .h file

--- ZiltoidLIB.h
+++ ZiltoidLIB.h
@@ -1,21 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-#include <string.h> // used by ReadTextFiles
-#include <math.h>
+#ifndef ZiltoidLIB_LOADED
+  #define ZiltoidLIB_LOADED 1
 
-#ifndef MAX_LINE_WIDTH
-	#define MAX_LINE_WIDTH 25000
+  #include <stdio.h>
+  #include <stdlib.h>
+
+  #include <string.h> // used by ReadTextFiles
+  #include <math.h>
+
+  #ifndef MAX_LINE_WIDTH
+    #define MAX_LINE_WIDTH 25000
+  #endif
+  #ifndef MAX_STR_L
+    #define  MAX_STR_L 200
+  #endif
+
+  #include "ReadTextFiles/ReadTextFiles.h"
+  #include "StringOperations/StringOperations.h"
+  #include "RandomNumbers/RandomNumbers.h"
+  #include "Interpolation/Interpolation.h"
+  #include "StructureFactor/StructureFactor.h"
+  #include "Mathematics/Mathematics.h"
 #endif
-#ifndef MAX_STR_L
-	#define  MAX_STR_L 200
-#endif
 
-#include "ReadTextFiles/ReadTextFiles.h"
-#include "StringOperations/StringOperations.h"
-#include "RandomNumbers/RandomNumbers.h"
-#include "Interpolation/Interpolation.h"
-#include "StructureFactor/StructureFactor.h"
-#include "Mathematics/Mathematics.h"
 
 
