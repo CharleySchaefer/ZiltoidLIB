e6a71597daa39ae48535e855b92dddc8afc66153
Charley Schaefer <charley.schaefer@gmail.com>
2018-07-03 12:32:07 +0100

    FIXED: Include randf2() in header file

--- RandomNumbers/RandomNumbers.h
+++ RandomNumbers/RandomNumbers.h
@@ -5,6 +5,7 @@
 int    randint(int);
 int    randint2(int, int);
 double randf(double);
+double randf2(double, double);
 double randn(void);
 double rand_dev_double(double, double, double, double);
 int    getRandConfig(int *, int, int *, int);
