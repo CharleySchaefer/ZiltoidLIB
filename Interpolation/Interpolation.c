ce8b12f73bf8189db1248458068cdc3481ddb82a
Charley Schaefer <charley.schaefer@gmail.com>
2018-09-20 15:29:03 +0100

    ADDED find_boundary_indices() and ADDED demo

--- Interpolation/Interpolation.c
+++ Interpolation/Interpolation.c
@@ -1,5 +1,48 @@
 #include "Interpolation.h"
 
+int find_boundary_indices(double *xarr, double *yarr, int N, double xint, int *indL, int *indU)
+{
+  int    nL=0, nU=N-1, n;
+  double x, xL, xU; // upper and lower x values
+  double f;
+  if (xint<(xL=xarr[nL]) | xint>(xU=xarr[nU])) // extrapolation needed
+    return(2);
+
+  if(xint==xL)
+  { *indL=0; *indU=0; }
+  else if (xint==xU)
+  { *indL=N-1; *indU=N-1; }
+  else
+  {
+    // find nL and nU such that nU=nL+1 and xarr[nL]<=xint<=xarr[nU]
+    while(nU-nL>1) 
+    {
+      n=(int)(nL+0.5*(float)(nU-nL));
+      x=xarr[n];
+      if((x<xL)|(x>xU)) // xarr not monotonically increasing!
+        {printf("ERROR: entries of xarr should be monotonically increasing!\n"); return(0);}
+      if (x==xint) // exact value found -> no interpolation needed -> done.
+      {
+        *indL=n;*indU=n;
+        return(1);
+      }
+      else if (x>xint) // update upper value
+      {
+        nU=n;
+        xU=x;
+      }
+      else if (x<xint) // update lower value
+      {
+        nL=n;
+        xL=x;
+      }
+    }
+    *indL=nL;
+    *indU=nU;
+  }
+  return(1);
+}
+
 int interp1(double *xarr, double *yarr, int N, double xint, double *yint)
 {
   int    nL=0, nU=N-1, n;
@@ -14,28 +57,10 @@ int interp1(double *xarr, double *yarr, int N, double xint, double *yint)
     *yint=yarr[N-1];
   else
   {
-    // find nL and nU such that nU=nL+1 and xarr[nL]<=xint<=xarr[nU]
-    while(nU-nL>1) 
+    if(!find_boundary_indices(xarr, yarr, N, xint, &nL, &nU))
     {
-      n=(int)(nL+0.5*(float)(nU-nL));
-      x=xarr[n];
-      if((x<xL)|(x>xU)) // xarr not monotonically increasing!
-        {printf("ERROR: entries of xarr should be monotonically increasing!\n"); return(0);}
-      if (x==xint) // exact value found -> no interpolation needed -> done.
-      {
-        *yint=yarr[n];
-        return(1);
-      }
-      else if (x>xint) // update upper value
-      {
-        nU=n;
-        xU=x;
-      }
-      else if (x<xint) // update lower value
-      {
-        nL=n;
-        xL=x;
-      }
+      printf("ERROR: find_boundary_indices() failed!\n"); 
+      return(0);
     }
 
     // interpolate
--- Interpolation/Interpolation.h
+++ Interpolation/Interpolation.h
@@ -2,4 +2,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int find_boundary_indices(double *, double *, int, double, int *, int *);
 int interp1(double *, double *, int, double, double *);
