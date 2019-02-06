0874bba8ef7e517c1d098ed6c5ec4a96f95cc592
Charley Schaefer <charley.schaefer@gmail.com>
2019-02-06 12:20:44 +0000

    ADDED StructureFactor3D plus demo

--- StructureFactor/StructureFactor.c
+++ StructureFactor/StructureFactor.c
@@ -79,3 +79,64 @@ int calculateStructureFactor(int NX, int NY, double dx, double **Psi2D, complex 
 
   return(1);
 }
+
+
+int calculateStructureFactor3D(int NX, int NY, int NZ, double dx, double ***Psi3D, complex ***Psi3D_FT, int Nbins, complex *buff1D, double *q_arr, double *SF)
+{
+  int i,j,k, ind;
+  int Nmin=(NX<NY ? (NX<NZ ? NX : NZ) : (NY<NZ ? NY : NZ));
+  double a, b, q,
+         dq=2.0*M_PI/(dx*Nmin), dq_binned,// qmax=sqrt(2.0)*M_PI*Nmin;
+         qmax=2.0*M_PI*(Nbins)/(sqrt(2.0)*((Nmin/2-1))*dx);
+  int *counter;
+
+  dq_binned=qmax/(Nbins-1); // dq in output file
+
+  // Initialise output
+  for(i=0; i<Nbins; i++)
+  {
+    q_arr[i]=dq_binned*i;
+    SF[i]=0.0;
+  }
+
+  // Obtain 2D fast Fourier transform (FFT)
+  for(i=0; i<NX; i++)
+    for(j=0; j<NY; j++)
+      for(k=0; k<NZ; k++)
+      Psi3D_FT[i][j][k] = (Psi3D[i][j][k]==1 ? 1 : (Psi3D[i][j][k]==2 ? -1 : Psi3D[i][j][k]) ); 
+
+  if(  isPowerOfTwo(NX) & isPowerOfTwo(NY) & isPowerOfTwo(NZ) )
+    fft3(Psi3D_FT, buff1D, NX, NY, NZ);
+  else
+  {
+    printf("Error: NX,NY,NZ should be powers of two in current implementation.\n");
+    return(-1);
+  }
+
+  // Take square of the FFT and take spherical average 
+  counter=(int*)buff1D;
+  for(i=0; i<Nbins; i++)
+    counter[i]=0;
+
+  for(i=0; i<Nmin/2; i++)
+  {
+    for(j=0; j<Nmin/2; j++)
+    {
+      for(k=0; k<Nmin/2; k++)
+      {
+        q = sqrt(i*i+j*j+k*k);
+        ind=(int)((double)q/(sqrt(2)));
+        a=creal(Psi3D_FT[i][j][k]);
+        b=cimag(Psi3D_FT[i][j][k]);
+        SF[ind] += ( a*a + b*b );
+        counter[ ind]++;
+      }
+    }
+  }
+
+  // normalise
+  for(i=0; i<Nbins; i++)
+    SF[i] = (counter[i]==0 ? 0.0 : SF[i]/counter[i]);
+
+  return(1);
+}
--- StructureFactor/StructureFactor.h
+++ StructureFactor/StructureFactor.h
@@ -6,4 +6,5 @@
 #include "../Mathematics/Fourier/Fourier.h"
 #include "../Mathematics/Mathematics.h"
 int calculateStructureFactor(int, int, double, double **, complex **, int, complex *, double *, double *);
+int calculateStructureFactor3D(int, int, int, double, double ***, complex ***, int, complex *, double *, double *);
 
