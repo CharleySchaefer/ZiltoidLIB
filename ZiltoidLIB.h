d11b45bb93e89037c66fffba6103fef3552f862e
Charley Schaefer <charley.schaefer@gmail.com>
2019-02-13 16:41:17 +0000

    ADDED applications

--- ZiltoidLIB.h
+++ ZiltoidLIB.h
@@ -16,8 +16,9 @@
 
   #include "ReadTextFiles/ReadTextFiles.h"
   #include "StringOperations/StringOperations.h"
-  #include "StructureFactor/StructureFactor.h"
   #include "Mathematics/Mathematics.h"
   #include "NumericalMethods/NumericalMethods.h"
   #include "kMC/VSSM.h"
+
+  #include "Applications/StructureFactor/StructureFactor.h"
 #endif
