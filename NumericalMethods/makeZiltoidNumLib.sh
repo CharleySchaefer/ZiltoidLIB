353704413432540dcd31a55086e8a99ac5fd2d0d
Charley Schaefer <charley.schaefer@gmail.com>
2019-03-22 12:38:42 +0000

    added license info

--- NumericalMethods/makeZiltoidNumLib.sh
+++ NumericalMethods/makeZiltoidNumLib.sh
@@ -1,3 +1,18 @@
+#   NumericalMethods is part of ZiltoidLIB.
+#   ZiltoidLIB: C library for physical modelling and simulation.
+#   Copyright 2018, Charley Schaefer (charley.schaefer@york.ac.uk)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 #!/bin/bash
 
 #===============================
