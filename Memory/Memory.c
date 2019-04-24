ab2887cd198b76ce99f70cd290407708e1114423
Charley Schaefer <charley.schaefer@gmail.com>
2019-04-24 11:25:56 +0100

    ADDED malloc and calloc functions

--- Memory/Memory.c
+++ Memory/Memory.c
@@ -1,5 +1,50 @@
 #include "Memory.h"
 
+//===============================================
+// 1D arrays
+int    *i1malloc(int N)
+{
+  int *arr=(int*)malloc(N*sizeof(int));
+  return arr;
+}
+float  *f1malloc(int N)
+{
+  float *arr=(float*)malloc(N*sizeof(float));
+  return arr;
+}
+double *d1malloc(int N)
+{
+  double *arr=(double*)malloc(N*sizeof(double));
+  return arr;
+}
+char   *c1malloc(int N)
+{
+  char *arr=(char*)malloc(N*sizeof(char));
+  return arr;
+}
+
+int    *i1calloc(int N)
+{
+  int *arr=(int*)calloc(N, sizeof(int));
+  return arr;
+}
+float  *f1calloc(int N)
+{
+  float *arr=(float*)calloc(N, sizeof(float));
+  return arr;
+}
+double *d1calloc(int N)
+{
+  double *arr=(double*)calloc(N, sizeof(double));
+  return arr;
+}
+char   *c1calloc(int N)
+{
+  char *arr=(char*)calloc(N, sizeof(char));
+  return arr;
+}
+
+
 void i1free(int *arr1)
 {
   if(arr1!=NULL)
@@ -29,6 +74,81 @@ void c1free(char *arr1)
     free(arr1); arr1=NULL;
   }
 }
+
+//===============================================
+// 2D arrays
+int    **i2malloc(int M, int N)
+{
+  int i;
+  int **arr=(int**)malloc(M*sizeof(int*));
+  for(i=0; i<M; i++)
+    arr[i]=i1malloc(N);
+  return arr;
+}
+
+float    **f2malloc(int M, int N)
+{
+  int i;
+  float **arr=(float**)malloc(M*sizeof(float*));
+  for(i=0; i<M; i++)
+    arr[i]=f1malloc(N);
+  return arr;
+}
+
+double    **d2malloc(int M, int N)
+{
+  int i;
+  double **arr=(double**)malloc(M*sizeof(double*));
+  for(i=0; i<M; i++)
+    arr[i]=d1malloc(N);
+  return arr;
+}
+
+char    **c2malloc(int M, int N)
+{
+  int i;
+  char **arr=(char**)malloc(M*sizeof(char*));
+  for(i=0; i<M; i++)
+    arr[i]=c1malloc(N);
+  return arr;
+}
+
+int    **i2calloc(int M, int N)
+{
+  int i;
+  int **arr=(int**)malloc(M*sizeof(int*));
+  for(i=0; i<M; i++)
+    arr[i]=i1calloc(N);
+  return arr;
+}
+
+float    **f2calloc(int M, int N)
+{
+  int i;
+  float **arr=(float**)malloc(M*sizeof(float*));
+  for(i=0; i<M; i++)
+    arr[i]=f1calloc(N);
+  return arr;
+}
+
+double    **d2calloc(int M, int N)
+{
+  int i;
+  double **arr=(double**)malloc(M*sizeof(double*));
+  for(i=0; i<M; i++)
+    arr[i]=d1calloc(N);
+  return arr;
+}
+
+char    **c2calloc(int M, int N)
+{
+  int i;
+  char **arr=(char**)malloc(M*sizeof(char*));
+  for(i=0; i<M; i++)
+    arr[i]=c1calloc(N);
+  return arr;
+}
+
 
 void i2free(int **arr2, int M)
 {
@@ -81,6 +201,84 @@ void c2free(char **arr2, int M)
     free(arr2); arr2=NULL;
   }
 }
+
+
+//===============================================
+// 2D arrays
+int    ***i3malloc(int L, int M, int N)
+{
+  int i;
+  int ***arr=(int***)malloc(M*sizeof(int**));
+  for(i=0; i<L; i++)
+    arr[i]=i2malloc(M, N);
+  return arr;
+}
+
+float    ***f3malloc(int L, int M, int N)
+{
+  int i;
+  float ***arr=(float***)malloc(M*sizeof(float**));
+  for(i=0; i<L; i++)
+    arr[i]=f2malloc(M, N);
+  return arr;
+}
+
+double    ***d3malloc(int L, int M, int N)
+{
+  int i;
+  double ***arr=(double***)malloc(M*sizeof(double**));
+  for(i=0; i<L; i++)
+    arr[i]=d2malloc(M, N);
+  return arr;
+}
+
+char    ***c3malloc(int L, int M, int N)
+{
+  int i;
+  char ***arr=(char***)malloc(M*sizeof(char**));
+  for(i=0; i<L; i++)
+    arr[i]=c2malloc(M, N);
+  return arr;
+}
+
+int    ***i3calloc(int L, int M, int N)
+{
+  int i;
+  int ***arr=(int***)malloc(M*sizeof(int**));
+  for(i=0; i<L; i++)
+    arr[i]=i2calloc(M, N);
+  return arr;
+}
+
+float    ***f3calloc(int L, int M, int N)
+{
+  int i;
+  float ***arr=(float***)malloc(M*sizeof(float**));
+  for(i=0; i<L; i++)
+    arr[i]=f2calloc(M, N);
+  return arr;
+}
+
+double    ***d3calloc(int L, int M, int N)
+{
+  int i;
+  double ***arr=(double***)malloc(M*sizeof(double**));
+  for(i=0; i<L; i++)
+    arr[i]=d2calloc(M, N);
+  return arr;
+}
+
+char    ***c3calloc(int L, int M, int N)
+{
+  int i;
+  char ***arr=(char***)malloc(M*sizeof(char**));
+  for(i=0; i<L; i++)
+    arr[i]=c2calloc(M, N);
+  return arr;
+}
+
+
+
 
 void i3free(int ***arr3, int L, int M)
 {
--- Memory/Memory.h
+++ Memory/Memory.h
@@ -2,22 +2,52 @@
 #define ZILTOID_MEMORY_H
 #include <stdlib.h> // alloc and free
 
-// free 1D array
-void i1free(int      *);
-void f1free(float    *);
-void d1free(double   *);
-void c1free(char     *);
+// 1D arrays size N
+int    *i1malloc(int);                  // int N
+float  *f1malloc(int);
+double *d1malloc(int);
+char   *c1malloc(int);
 
-// free 2D array
-void i2free(int     **, int);
-void f2free(float   **, int);
-void d2free(double  **, int);
-void c2free(char    **, int);
+int    *i1calloc(int);                    
+float  *f1calloc(int);
+double *d1calloc(int);
+char   *c1calloc(int);
 
-// free 3D array
-void i3free(int    ***, int, int);
-void f3free(float  ***, int, int);
-void d3free(double ***, int, int);
-void c3free(char   ***, int, int); 
+void    i1free(int      *);
+void    f1free(float    *);
+void    d1free(double   *);
+void    c1free(char     *);
+
+// 2D arrays size MxN
+int    **i2malloc(int, int);            // int M, int N
+float  **f2malloc(int, int);
+double **d2malloc(int, int);
+char   **c2malloc(int, int);
+
+int    **i2calloc(int, int);
+float  **f2calloc(int, int);
+double **d2calloc(int, int);
+char   **c2calloc(int, int);
+
+void     i2free(int     **, int);       // int M
+void     f2free(float   **, int);
+void     d2free(double  **, int);
+void     c2free(char    **, int);
+
+// 3D arrays size LxMxN
+int    ***i3malloc(int, int, int);      // int L, int M, int N
+float  ***f3malloc(int, int, int);
+double ***d3malloc(int, int, int);
+char   ***c3malloc(int, int, int);
+
+int    ***i3calloc(int, int, int);
+float  ***f3calloc(int, int, int);
+double ***d3calloc(int, int, int);
+char   ***c3calloc(int, int, int);
+
+void      i3free(int    ***, int, int); // int L, int M
+void      f3free(float  ***, int, int);
+void      d3free(double ***, int, int);
+void      c3free(char   ***, int, int);
 
 #endif // ZILTOID_MEMORY_H
