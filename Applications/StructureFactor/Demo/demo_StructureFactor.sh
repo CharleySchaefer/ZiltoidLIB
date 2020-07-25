ee5c2d6da27b2bf8e5dbb4673124a37467821233
CharleySchaefer <charley.schaefer@york.ac.uk>
2020-07-25 02:59:36 +0200

    fixed g++ option

--- Applications/StructureFactor/Demo/demo_StructureFactor.sh
+++ Applications/StructureFactor/Demo/demo_StructureFactor.sh
@@ -5,8 +5,25 @@ MainC="Demo_StructureFactorAndCorrelationFunction.c"
 executable="StructureFactorAndCorrelationFunction.o"
 
 # Compile
+CC=gcc # default
+arg=""
+debug=""
+
+for n in "$@" ; do
+  if [ $n == "-g" ]; then
+    echo "  Debugging mode."
+    debug="-g"
+  elif [ $n == "--gcc" ]; then
+    echo "  Using gcc"
+    CC=gcc
+  elif [ $n == "--g++" ]; then
+    echo "  Using g++"
+    CC=g++
+  fi
+done
+
 echo "  Compiling $executable"
-./compile_StructureFactor.sh
+./compile_StructureFactor.sh --$CC $debug
 
 inputfile=psi_example.dat
 LX=10.0
